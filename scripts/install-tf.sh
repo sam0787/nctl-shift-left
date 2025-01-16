0cd15af12efb22ded1eaac88d017df2f0548901f
Sachin Agarwal <sachinagarwal@Sachins-MacBook-Pro.local>
2025-01-16 13:54:17 +0530

    fix: 🐛 Fixing the plan command
    BREAKING CHANGE: 🧨 No

--- scripts/install-tf.sh
+++ scripts/install-tf.sh
@@ -21,7 +21,7 @@ run_terraform_commands() {
     # Plan the changes Terraform will make
     terraform plan -out tfplan.binary
     # Store in JSON
-    terraform show -json tfplan.binary | jq > payload.plan
+    terraform show -json tfplan.binary | jq > payload.json
     echo "Terraform commands executed successfully."
 }
 
